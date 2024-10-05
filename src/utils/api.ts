e0d6eff030221775fb818973777d7b7fc8f994f4
softK1T <nazar12522@gmail.com>
2024-10-05 21:31:22 +0200

    Fixed path

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -8,7 +8,7 @@ export async function callOpenAI(
   }
 ): Promise<string> {
   try {
-    const res = await fetch("http://localhost:8000/chat", {
+    const res = await fetch("http://127.0.0.1:8000/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message, ...options }),
